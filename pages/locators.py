084dc623d819cb80f99a4090f93df0e4bf498866
Natalia <wel0lentasch@gmail.com>
2022-01-29 22:39:34 +0300

    add tests fot product page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     REGISTER_FORM = (By.CSS_SELECTOR, '#login_form')
     LOGIN_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    BOOK_PRICE = (By.CSS_SELECTOR, '.price_color')
+    BOOK_NAME = (By.CSS_SELECTOR, '.breadcrumb .active')
+    ADD_MESSAGE_NAME = (By.CSS_SELECTOR, '.alert:nth-child(1)')
+    ADD_MESSAGE_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket_button.click()
+
+    def name_for_product_in_basket(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        add_message_name = self.browser.find_element(*ProductPageLocators.ADD_MESSAGE_NAME).text
+        assert book_name in add_message_name, 'book name in basket is not correct'
+
+    def price_for_product_in_basket(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        add_message_price = self.browser.find_element(*ProductPageLocators.ADD_MESSAGE_PRICE).text
+        assert book_price in add_message_price, 'book price in basket is not correct'
